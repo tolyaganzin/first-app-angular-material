f678027df817ca3d3a282627ab09b6ffa1dd5058
tolyaganzin <tolyaganzin@mail.ru>
2016-09-23 15:02:15 +0300

    clever menu

--- app/menu.controller.js
+++ app/menu.controller.js
@@ -2,18 +2,27 @@
 angular.module( 'amApp', [ 'ngMaterial' ] );
 
 angular.module('amApp')
-  .controller('menuController',[
-    function () {
-      this.isOpen = false;
+  .controller('menuController',['$mdSidenav', '$mdMedia', '$scope',
+    function ($mdSidenav, $mdMedia, $scope) {
+      this.isOpen=!$mdMedia('gt-sm');
+    //  console.log(this.isOpen);
+      $scope.$watch(function() { return $mdMedia('gt-sm') ; }, angular.bind(this,function(isSm) {
+        this.isOpen = isSm;
+        console.log(this.isOpen);
+      }));
+
+
       this.menuImg = 'menu.svg';
       this.menu = function(){
-        if(this.isOpen) {
-          this.isOpen = false;
-          this.menuImg = 'menu.svg';
-        }
-        else {
-          this.isOpen = true;
-          this.menuImg = 'keyboard_backspace.svg';
-        }
-      };
+          if(this.isOpen) {
+            this.isOpen=false;
+            this.menuImg = 'menu.svg';
+          }
+          else {
+            this.isOpen=true;
+            this.menuImg = 'more_vert.svg';
+          }
+        };
+
+
     }]);
