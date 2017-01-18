91499b9f3bb8da5cbff4ad6aa45b33719f4fc055
tolyaganzin <tolyaganzin@mail.ru>
2017-01-18 11:03:56 +0200

    add description to readme and deleted comented code

--- README.md
+++ README.md
@@ -1,1 +1,39 @@
-first app angular material
+###My first app angular-material (user list with more description about user)
+#This app has not clear code. The app just worked
+##Nginx config
+```
+server {
+	#listen 80 default_server;
+	#listen [::]:80 default_server;
+
+	root /var/www/projects/angular-material;
+	index index.php index.html index.htm index.nginx-debian.html;
+
+	server_name angular-material.dev www.angular-material.dev;
+
+	location / {
+		# First attempt to serve request as file, then
+		# as directory, then fall back to displaying a 404.
+        	try_files $uri $uri/ /index.html =404;
+	}
+
+	# pass the PHP scripts to FastCGI server listening on 127.0.0.1:9000
+	#
+	location ~ \.php$ {
+		include snippets/fastcgi-php.conf;
+
+		# With php7.0-cgi alone:
+		#fastcgi_pass 127.0.0.1:9000;
+		# With php7.0-fpm:
+		fastcgi_pass unix:/run/php/php7.0-fpm.sock;
+	}
+
+	# deny access to .htaccess files, if Apache's document root
+	# concurs with nginx's one
+	#
+	#location ~ /\.ht {
+	#	deny all;
+	#}
+}
+```
+### Look my next better app with Angular JS and yii2: https://github.com/tolyaganzin/angular-yii2
--- app/menu.controller.js
+++ app/menu.controller.js
@@ -2,32 +2,53 @@
 angular.module( 'amApp', [ 'ngMaterial' ] );
 
 angular.module('amApp')
-  .controller('menuController',['$mdSidenav', '$mdMedia', '$scope',
-    function ($mdSidenav, $mdMedia, $scope) {
+  .controller('menuController',['$mdSidenav', '$mdMedia', '$scope', '$mdBottomSheet',
+    function ($mdSidenav, $mdMedia, $scope, $mdBottomSheet) {
+
+
+      this.users=[
+        {
+          name: 'Thomas Burleson',
+          avatar: 'android.svg',
+          content: 'Bacon ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+        },
+        {
+          name: 'Aron Frost',
+          avatar: 'account.svg',
+          content: 'Chease ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+        }
+      ];
+      this.selected=this.users[0];
       this.isOpen=!$mdMedia('gt-sm');
-    //  console.log(this.isOpen);
+
       $scope.$watch(function() { return $mdMedia('gt-sm'); }, angular.bind(this,function(isSm) {
         this.isOpen = isSm;
-        if(this.menuImg)
+        if(this.isOpen)
           this.menuImg = 'menu.svg';
         console.log(this.isOpen);
       }));
 
       this.menuImg = 'menu.svg';
 
-      this.menu = function(){
-          if(this.isOpen) {
-            this.isOpen=false;
-            this.menuImg = 'menu.svg';
-          }
-          else {
-            this.isOpen=true;
-            this.menuImg = 'more_vert.svg';
-          }
-        };
+      this.selectUser=function(user){
+        this.selected=user;
+      };
 
-      // this.menu = function() {
-      //   $mdSidenav('left').toggle();
-      // };
+      this.share=function (selectedUser) {
+        $mdBottomSheet.show({
+          template: '<md-bottom-sheet><h3>Hello, '+selectedUser.name+'!</h3></md-bottom-sheet>',
+          parent: '#content'
+        });
+        function UserSheetController(){
+          this.user=selectedUser;
+          this.performAction=function (action) {
+            $mdBottomSheet.hide();
+          };
+        }
+      };
+
+      this.menu = function() {
+        $mdSidenav('left').toggle();
+      };
 
     }]);
